f0bc9e5692709b6dda13dd90cecacc6f2c408ac5
Rebecca GERREY <rebeccag@live.fr>
2023-04-20 17:55:22 +0200

    Added system to see favorites Recipes for user on his profil.

--- assets/js/_user-favoriteRecipes.js
+++ assets/js/_user-favoriteRecipes.js
@@ -1,17 +1,17 @@
-const userShowFavoriteRecipesButton = document.querySelectorAll('.js_user_show_favoriteRecipes_button');
+const userShowFavoriteRecipesButton = document.querySelector('#js_user_show_favoriteRecipes_button');
 const userFavoriteRecipes = document.querySelectorAll('.js_user_favoriteRecipes');
 
 // Fonction pour écouter l'évènement du clic sur le boutton.
 function listenEvents() {
-    userShowFavoriteRecipesButton.forEach((button, index) => {
-        button.addEventListener('click', () => showUserFavoriteRecipes(index));
-    });
-}
+    userShowFavoriteRecipesButton.addEventListener('click', showUserFavoriteRecipes);
+};
 
 // Méthode pour afficher le formulaire d'ajout de commentaire spécifique à la recette.
-function showUserFavoriteRecipes(index) {
-    userFavoriteRecipes[index].classList.toggle('hide');
-    userFavoriteRecipes[index].classList.toggle('show');
-}
+function showUserFavoriteRecipes() {
+    userFavoriteRecipes.forEach(recipe => {
+        recipe.classList.toggle('hide');
+        recipe.classList.toggle('show');
+    })
+};
 
 listenEvents();
