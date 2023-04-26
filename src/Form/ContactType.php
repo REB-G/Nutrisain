f5532533857cbcdc13540d7e326e437eeb50592a
Rebecca GERREY <rebeccag@live.fr>
2023-04-26 09:28:42 +0200

    Contact page + contact-form for CRUD admin styled.

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,17 +6,75 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('phoneNumber')
-            ->add('adress')
-            ->add('email')
-            ->add('pageTitle')
-            ->add('pageText')
+            ->add('pageTitle', TextType::class, [
+                'row_attr' => [
+                    'class' => 'contact-page-form__field'
+                ],
+                'label' => 'Titre de la page',
+                'label_attr' => [
+                    'class' => 'contact-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'contact-page-form__field--input',
+                ]
+            ])
+            ->add('pageText', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'contact-page-form__field'
+                ],
+                'label' => 'Texte d\'information',
+                'label_attr' => [
+                    'class' => 'contact-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'contact-page-form__field--input',
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'row_attr' => [
+                    'class' => 'contact-page-form__field'
+                ],
+                'label' => 'Téléphone',
+                'label_attr' => [
+                    'class' => 'contact-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'contact-page-form__field--input',
+                ]
+            ])
+            ->add('adress', TextType::class, [
+                'row_attr' => [
+                    'class' => 'contact-page-form__field'
+                ],
+                'label' => 'Adresse',
+                'label_attr' => [
+                    'class' => 'contact-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'contact-page-form__field--input',
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'row_attr' => [
+                    'class' => 'contact-page-form__field'
+                ],
+                'label' => 'Email',
+                'label_attr' => [
+                    'class' => 'contact-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'contact-page-form__field--input',
+                ]
+            ])
         ;
     }
 
