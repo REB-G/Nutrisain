c12b29a7b1a027aafe0d7d35afc8500584cb7efd
Rebecca GERREY <rebeccag@live.fr>
2023-04-27 09:36:23 +0200

    EditPasswordUser Form + editUserProfil form created.

--- src/Form/EditUserProfilType.php
+++ src/Form/EditUserProfilType.php
@@ -6,6 +6,8 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EditUserProfilType extends AbstractType
@@ -13,10 +15,50 @@ class EditUserProfilType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('name')
-            ->add('firstname')
-            ->add('Valider', SubmitType::class)
+            ->add('name', TextType::class, [
+                'row_attr' => [
+                    'class' => 'change-user-informations-form__field'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'change-user-informations-form__field--label'
+                ],
+                'attr' => [
+                    'placeholder' => 'Nom',
+                    'class' => 'change-user-informations-form__field--input',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'row_attr' => [
+                    'class' => 'change-user-informations-form__field'
+                ],
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'change-user-informations-form__field--label'
+                ],
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                    'class' => 'change-user-informations-form__field--input',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'row_attr' => [
+                    'class' => 'change-user-informations-form__field'
+                ],
+                'label' => 'Email',
+                'label_attr' => [
+                    'class' => 'change-user-informations-form__field--label'
+                ],
+                'attr' => [
+                    'placeholder' => 'Email',
+                    'class' => 'change-user-informations-form__field--input',
+                ],
+            ])
+            ->add('Valider', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn__linear-gradient',
+                ],
+            ])
         ;
     }
 
