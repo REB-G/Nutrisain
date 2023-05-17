4f8fa2613eb70bc57170d7b5da590abb1999cbcd
Rebecca GERREY <rebeccag@live.fr>
2023-05-17 11:07:54 +0200

    Fix style for phone in all forms + navbar modified for admin.

--- assets/js/_navbar.js
+++ assets/js/_navbar.js
@@ -1,5 +1,27 @@
-const navBar = document.querySelector('.header__container--nav');
+const navBar = document.querySelector('#js_header_container_nav');
 const navBtn = document.querySelector('.header__container--btn i');
+const burger = document.querySelector('.header__container--btn');
+const adminLink = document.querySelector('#js_admin_link');
+const headerContainer = document.querySelector('#js_header_container');
+const headerLogo = document.querySelector('#js_header_logo');
+
+if (adminLink) {
+    burger.classList.add('show');
+    headerLogo.classList.add('header__admin-container--logo');
+    headerLogo.classList.remove('header__container--logo');
+    headerContainer.classList.add('header__admin-container');
+    headerContainer.classList.remove('header__container');
+    navBar.classList.add('header__admin-container--nav');
+    navBar.classList.remove('header__container--nav');
+    navBar.classList.add('hide');
+    navBar.classList.remove('nav-active');
+    navBtn.classList.remove('fa-times');
+    navBtn.classList.add('fa-bars');
+    navBtn.addEventListener('click', function() {
+        navBar.classList.toggle('hide');
+        toggleNav();
+    });
+}
 
 function toggleNav() {
     navBtn.classList.toggle('fa-bars');
