ddfd3601c11750227c84b365cef2a04f7de5851a
Rebecca GERREY <rebeccag@live.fr>
2023-05-22 11:17:54 +0200

    Fix firstConnexionType.

--- src/Form/FirstConnexionUserType.php
+++ src/Form/FirstConnexionUserType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class FirstConnexionUserType extends AbstractType
 {
@@ -27,9 +28,13 @@ class FirstConnexionUserType extends AbstractType
                             'message' => 'Veuillez saisir un mot de passe.',
                         ]),
                         new Length([
-                            'min' => 6,
-                            'minMessage' => 'Votre mot de passe doit contenir au moins 6 caractères.',
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins 8 caractères.',
                             'max' => 4096,
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+                            'message' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.'
                         ]),
                     ],
                     'row_attr' => [
