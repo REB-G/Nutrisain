70c71d441087e88208c7bf77b3b557613db016d2
Rebecca GERREY <rebeccag@live.fr>
2023-05-22 11:13:40 +0200

    Added more security when login + on password.

--- src/Form/EditPasswordUserType.php
+++ src/Form/EditPasswordUserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EditPasswordUserType extends AbstractType
 {
@@ -25,10 +26,14 @@ class EditPasswordUserType extends AbstractType
                             'message' => 'Veuillez saisir un mot de passe.',
                         ]),
                         new Length([
-                            'min' => 6,
-                            'minMessage' => 'Votre mot de passe doit contenir au moins 6 caractères.',
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins 8 caractères.',
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).+$/',
+                            'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.'
                         ]),
                     ],
                     'row_attr' => [
